45eed00c1820d84260fe2cd9ac72acc60db312e3
Wes Glassmeyer <71471439+WesGlassmeyer@users.noreply.github.com>
2020-10-22 18:57:37 -0600

    Add files via upload
    Initial upload of MVP

---
+++ index.js
@@ -0,0 +1,216 @@
+'use strict';
+
+const searchURL = "https://wger.de/api/v2/exercise/?format=json&language=2"
+
+let randomExerciseNumber = [] 
+let userCreatedWorkout = []
+let responseLength = []
+let exercises = []
+
+//---These functions are for the user created workout---
+
+function displayResults(responseJson) {
+  $('#results-list').empty();
+  for (let i = 0; i < responseJson.results.length; i++){
+    $('#results-list').append(
+      `<input type="radio" id="exercise" name="exercise" value= '${responseJson.results[i].name}' checked>
+      <label>${responseJson.results[i].name}</label><br>
+      <p>${responseJson.results[i].description}</p>`
+    )
+   };
+   if (responseJson.results.length === 0) {
+     $('#results-list').append(`<li><h3>No results based on this combination</h3></li>`)
+   } 
+  $('#results').removeClass('hidden');
+};
+
+function getExercise(searchCategory, searchEquipment) {
+  const categoryFormat = "&category=" + searchCategory
+  const equipmentFormat = "&equipment=" + searchEquipment
+  const url = searchURL + categoryFormat + equipmentFormat
+
+fetch(url)
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error(response.statusText);
+    })
+    .then(responseJson => displayResults(responseJson))
+    .catch(err => {
+      $('#js-error-message').text(`Something went wrong: ${err.message}`);
+    });
+}
+
+function displayUserCreatedWorkout(userCreatedWorkout) {
+  $('#user-created-list').empty()
+  for (let i = 0; i < userCreatedWorkout.length; i++) {
+    $('#user-created-list').append(
+      `<li>${userCreatedWorkout[i]}</li>`
+    )
+  }
+  $('#workout').removeClass('hidden2');
+}
+
+//---These functions are for the random workout creator---
+
+function displayRandomExercises(exercises) {
+  $('#random-results-list').empty();
+  for (let i = 0; i < exercises.length; i++) {
+    $('#random-results-list').append(
+      `<li>${exercises[i].name}</li>
+      <p>${exercises[i].description}</p>`
+    )
+  }
+  $('#random-results').removeClass('hidden')
+}
+
+function getArmExercises() {
+ return fetch('https://wger.de/api/v2/exercise/?format=json&language=2&limit=100&category=8')
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error(response.statusText);
+    })
+    .then(response => response)
+}
+
+function getLegExercises() {
+ return fetch('https://wger.de/api/v2/exercise/?format=json&language=2&limit=100&category=9')
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error(response.statusText);
+    })
+    .then(response => response)
+}
+
+function getAbsExercises() {
+ return fetch('https://wger.de/api/v2/exercise/?format=json&language=2&limit=100&category=10')
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error(response.statusText);
+    })
+    .then(response => response)
+}
+
+function getChestExercises() {
+ return fetch('https://wger.de/api/v2/exercise/?format=json&language=2&limit=100&category=11')
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error(response.statusText);
+    })
+    .then(response => response)
+}
+
+function getBackExercises() {
+ return fetch('https://wger.de/api/v2/exercise/?format=json&language=2&limit=100&category=12')
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error(response.statusText);
+    })
+    .then(response => response)
+}
+
+function getShoulderExercises() {
+ return fetch('https://wger.de/api/v2/exercise/?format=json&language=2&limit=100&category=13')
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error(response.statusText);
+    })
+    .then(response => response)
+}
+
+function getCalvesExercises() {
+ return fetch('https://wger.de/api/v2/exercise/?format=json&language=2&limit100&category=14')
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error(response.statusText);
+    })
+    .then(response => response)
+}
+
+function getAllExercises() {
+ return Promise.all([getArmExercises(), getLegExercises(), getAbsExercises(), getChestExercises(), getBackExercises(), getShoulderExercises(), getCalvesExercises()])
+ .then(responses => { 
+   for (let i = 0; i < responses.length; i++) {
+     responseLength.push(responses[i].results.length)
+   }
+  return responses
+  })
+  .then(responses => {
+    getRandomNumber()
+    return responses
+    
+  })
+  .then(responses => {
+    for (let i = 0; i < responses.length; i++) {
+      exercises.push(responses[i].results[randomExerciseNumber[i]])
+    }
+  })
+  
+  .then( () =>  {
+    displayRandomExercises(exercises)
+  })
+}
+
+function getRandomNumber() {
+  for (let i = 0; i < responseLength.length; i++) {
+    randomExerciseNumber.push(Math.floor(Math.random() * (responseLength[i] - 1 )))
+  }
+}
+
+//----These functions are the event listeners---
+
+function createWorkout() {
+  $('form').submit(event => {
+    event.preventDefault();
+    const searchCategory = $('#js-search-category').val();
+    const searchEquipment = $('#js-search-equipment').val();
+   getExercise(searchCategory, searchEquipment);
+  });
+}
+
+function addToWorkout() {
+  $('#add').click(event => {
+    event.preventDefault();
+    userCreatedWorkout.push($('#exercise:checked').val())
+    displayUserCreatedWorkout(userCreatedWorkout)
+  })
+}
+
+function resetWorkout() {
+  $('#reset').click(event => {
+    event.preventDefault();
+    userCreatedWorkout.splice(0,userCreatedWorkout.length)
+    $('#user-created-list').empty()
+    $('#workout').addClass('hidden2')
+  })
+}
+
+function createRandomWorkout() {
+  $('#random-workout').click(event => {
+    event.preventDefault()
+    getAllExercises()
+   randomExerciseNumber.splice(0,randomExerciseNumber.length)
+   responseLength.splice(0,responseLength.length)
+   exercises.splice(0,exercises.length)
+  })
+}
+
+$(createWorkout);
+$(addToWorkout);
+$(resetWorkout)
+$(createRandomWorkout)
